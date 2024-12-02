a8ac7a59cabdcc31baa733f26fd3970ae5aa5ea8
McThicc <stevenjonesjr0729@gmail.com>
2024-12-01 23:02:44 -0500

    potential sign up and login incoming

---
+++ routes/loginRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+
+// Sign-up route
+router.post('/signup', async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email already in use' });
+    }
+
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error creating user' });
+  }
+});
+
+module.exports = router;
